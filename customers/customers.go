3b72f9c38e6e3622f16dd545dd6c1acc803a7178
仗义小英雄 <108519259+tzh001234@users.noreply.github.com>
2022-12-19 20:48:48 +0800

    Update customers.go

--- customers/customers.go
+++ customers/customers.go
@@ -1,1 +1,55 @@
+package customers
+
+import (
+	"HomeWork/customers_/customer"
+) 
+
+
+type Customers struct{
+	CustomersSlice []customer.Customer
+}
+
+func (c *Customers)All() []customer.Customer{
+	return c.CustomersSlice
+}
+
+func (c *Customers)Add(na string,s string,a int,p string,e string){
+	ccc:=customer.NewCustomer(na,s,a,p,e)
+	c.CustomersSlice=append(c.CustomersSlice,ccc)
+} 
+func (c *Customers)Delete(n int)int{
+	q:=c.chazhaoxiabiao(n)
+	if q!=-1{
+		c.CustomersSlice=append(c.CustomersSlice[:q],c.CustomersSlice[q+1:]... )
+	}
+	return q
+}
+func (c *Customers)chazhaoxiabiao(n int)int{
+	q:=-1
+	for i:=0;i<len(c.CustomersSlice);i++{
+		if c.CustomersSlice[i].Number==n{
+			q=i 
+		}
+	}
+	return q 
+}
+
+func (c *Customers)Amend(n int)int{
+	
+	return c.chazhaoxiabiao(n)
+
+}
+
+
+func NewCustomers()Customers{
+	
+	customers:=Customers{
+	}
+	c:=customer.NewCustomer("田子恒","男",18,"110","1186@qq.com")
+
+	customers.CustomersSlice=append(customers.CustomersSlice,c)
+	
+	return customers
+		
+}
 
